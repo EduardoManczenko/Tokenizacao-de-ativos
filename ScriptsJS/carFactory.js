7b872456ca6565ec701f2c1fab86a00d4698a845
Nosferas <eduardo.manczenko@gmail.com>
2022-10-20 01:23:45 -0300

    Commit

--- ScriptsJS/carFactory.js
+++ ScriptsJS/carFactory.js
@@ -3,16 +3,19 @@ let newSymbol = document.getElementById('newSymbol')
 let newImg = document.getElementById('newImg')
 let newPrice = document.getElementById('newPrice')
 let newSupply = document.getElementById('newSupply')
+let feesSpan = document.getElementById('fees')
+
 
 async function mintCar(){
     const provider = getProvider()
     const signer = provider.getSigner()
     const contract = new ethers.Contract(carFactory, [createCar], provider)
     const contractSigner = contract.connect(signer)
+    feesSpan.innerHTML = "Aguarde 1 de 2 transaçoes..."
     const create = await contractSigner.create(newNome.value, newSymbol.value, newPrice.value, newSupply.value, newImg.value)
     
     await create.wait(1)
-
+    
     //provider.waitForTransaction(create.hash, 1, 150000)
 
 
@@ -21,14 +24,15 @@ async function mintCar(){
    //TODO: PERMISSAO DEPOIS QUE A TRANSAÇÃO FOR MINEIRADA
     console.log(create.hash)
     const receip = await provider.getTransactionReceipt(create.hash.toString())
-    console.log(create.log)
-
-    console.log(receip)
     
     let add = ''
     await Promise.all([receip]).then(res => add = res[0].logs[0].address)
     console.log(add, "aqyuu")
+    feesSpan.innerHTML = "Aguarde 2 de 2 transaçoes..."
     await permToBank(add)
+    feesSpan.innerHTML = "Concluido!"
+
+    
 }
 
 async function permToBank(address){
@@ -37,5 +41,6 @@ async function permToBank(address){
     const contract = new ethers.Contract(address, [increaseAllowance], provider)
     const contractSigner = contract.connect(signer)
     const perm = await contractSigner.increaseAllowance(address, ethers.utils.parseUnits(newSupply.value))
+    await perm.wait(1)
     console.log(perm)
 }
